596024035646f6c3a1e8121253bda4b2a0eefbab
FrancescoPanza <94964637+FrancescoPanza@users.noreply.github.com>
2023-06-09 12:45:58 +0200

    Aggiunti test vari

--- src/main/java/ContaVocali.java
+++ src/main/java/ContaVocali.java
@@ -1,9 +1,20 @@
+import java.security.InvalidParameterException;
+
 public class ContaVocali {
     public static int conteggioVocaliSottostringa(String input, char letteraInizio, char letteraFine) {
         int numeroVocali = 0;
         boolean trovataInizio = false;
 
         input = input.toLowerCase();
+        letteraInizio = Character.toLowerCase(letteraInizio);
+        letteraFine = Character.toLowerCase(letteraFine);
+
+
+        if(input.isEmpty()){
+            throw new RuntimeException("Stringa non inserita");
+        }
+
+
 
         for (int i = 0; i < input.length(); i++) {
             char carattere = input.charAt(i);
--- src/test/java/ContaVocaliTest.java
+++ src/test/java/ContaVocaliTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,20 +14,50 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ContaVocaliTest {
     @Test
-    @DisplayName("TestConValoriValidi")
-    void ValoriValidi() {
+    @DisplayName("mancato inserimento della stringa")
+    void StringaNulla() {
+        assertThrows(RuntimeException.class,
+                () -> ContaVocali.conteggioVocaliSottostringa(null,'a', 'b'));
 
-        Assertions.assertEquals(6, ContaVocali.conteggioVocaliSottostringa("alfa", "a", "b"));
+        }
+
+    @Test
+    public void testEccezioneStringaVuota() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ContaVocali.conteggioVocaliSottostringa("", 'a', 'b');
+        });
     }
 
 
 
-
-    @ParameterizedTest // prove
+    @ParameterizedTest
     @CsvSource({
-            "articolo, 00.0, 1 , "
+            "stringadiprova, i, o, 4 ",
+            "stringadiprova, s, p, 3 ",
+            "stringadiprova, a, v, 3",
+/*
+            "stringadiprova, e, v, 0",
+            "stringadiprova, i, v, 4",
+            "stringadiprova, o, v, 1",
+            "stringadiprova, u, v, 0",
+*/
+            "stringadiprova, t, a, 2",
+            " stringadiprova, t, a, 2",
+            "stringa di prova, t, a, 2",
+            "stringa di prova,' ',' ', 0",
+            "stringa di prova,' ', i, 1",
+            "stringa di prova, t,' ', 2",
+            "345sfrio5656gihj96, 5, o, 2",
+            "345sfrio5656gihj96i, 5, 9, 3",
+            "34596, 3, 6, 0",
+            "stringadiprovAdue, i, A, 2",
+            "as/jdirc@efhudfg?z#, /, @, 1"
+
     })
-    void testParam(String nomeArticolo, double prezzoArticolo, int quantitaArticolo, List<Articolo> articoli ){
-        Calcolo myCalc = new Calcolo();
+
+    @DisplayName("Test sugli input")
+    void testParam(String stringa, char inizio, char fine, int nroVocaliGiusto){
+        assertEquals(nroVocaliGiusto, ContaVocali.conteggioVocaliSottostringa(stringa, inizio, fine));
     }
+
 }
